6209fd6f364868a0cd1e1881b88663fe471690b6
Pedro Gonzalez <pdro74@hotmail.com>
2025-08-17 12:27:48 +0200

    replace manual REST implementation with Spring REST dependency. New api path=api_rest

--- src/main/java/com/pgf/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/pgf/cruddemo/dao/EmployeeRepository.java
@@ -3,7 +3,7 @@ package com.pgf.cruddemo.dao;
 import com.pgf.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-// JPA interface to replace manual DAO implementation in latest commit
+// JPA interface to replace manual DAO implementation in earlier commits
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     // provides all CRUD methods, no need to create an implementation
 }
